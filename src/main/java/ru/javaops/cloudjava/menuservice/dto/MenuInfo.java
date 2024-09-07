549b6a2bb56cdc0218e5fde5d5f1f4bc109b7006
gkislin <javawebinar@yandex.ru>
2024-09-07 12:22:23 +0300

    6_orders_info_1

---
+++ src/main/java/ru/javaops/cloudjava/menuservice/dto/MenuInfo.java
@@ -0,0 +1,16 @@
+package ru.javaops.cloudjava.menuservice.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
+public class MenuInfo {
+    private String name;
+    private BigDecimal price;
+    private Boolean isAvailable;
+}
