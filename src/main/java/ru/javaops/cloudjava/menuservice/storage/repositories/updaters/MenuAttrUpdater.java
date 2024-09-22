8a306034879b772f0a4915d56dd344ce24b44527
Дмитрий Поплаухин <Poplaukhin.D.S@sbertech.ru>
2024-09-22 19:35:29 +0300

    [+] reflect updateAttr

--- src/main/java/ru/javaops/cloudjava/menuservice/storage/repositories/updaters/MenuAttrUpdater.java
+++ src/main/java/ru/javaops/cloudjava/menuservice/storage/repositories/updaters/MenuAttrUpdater.java
@@ -5,8 +5,23 @@ import lombok.AllArgsConstructor;
 import ru.javaops.cloudjava.menuservice.dto.UpdateMenuRequest;
 import ru.javaops.cloudjava.menuservice.storage.model.MenuItem;
 
+import java.util.Arrays;
+
 @AllArgsConstructor
 public class MenuAttrUpdater<V> {
     public void updateAttr(CriteriaUpdate<MenuItem> criteria, UpdateMenuRequest dto) {
+        Arrays.stream(dto.getClass()
+                .getDeclaredFields())
+                .forEach(el -> {
+                    el.setAccessible(true);
+                    try {
+                        if (el.get(dto.getClass()) != null) {
+                            criteria.set(el.getName(), el.get(dto));
+                        }
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e.getMessage());
+                    }
+                }
+        );
     }
 }
