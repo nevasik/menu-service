85c9088205e9f2856b1bd6782f4b0c7c741ff696
Дмитрий Поплаухин <Poplaukhin.D.S@sbertech.ru>
2024-09-22 10:27:15 +0300

    [+] apply patch 1_data_layer_3

--- src/main/java/ru/javaops/cloudjava/menuservice/MenuServiceApplication.java
+++ src/main/java/ru/javaops/cloudjava/menuservice/MenuServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MenuServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MenuServiceApplication.class, args);
 	}
